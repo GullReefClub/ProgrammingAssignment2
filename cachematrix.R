f18e1904935c5e6c2cb6832a471812a6566ff115
robertdiley <robert.diley@ruralsourcing.com>
2016-10-06 21:44:58 -0400

    initial submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# The functionality submitted is closely tied to the template code given in the 
+# assignment. To achieve working code only the function calls needed changing
+# (i.e., replacing "mean" with "solve".)  Describing what happens is more 
+# interesting.
+ 
+# The first function, makeCacheMatrix, is used to instantiate an object in R 
+# which is similar to a 'class' in modern OOP languages. It contains 4 
+# behaviors which are analogous to 'methods' of a 'class' under the OOP paradigm.
+# One of these stores the input parameter
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
